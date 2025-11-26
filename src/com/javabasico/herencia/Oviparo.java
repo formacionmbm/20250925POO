485d51f9be80f6a4121a6800083529f667162d16
Marcos <viacepeda@gmail.com>
2025-11-26 11:23:18 +0100

    Inicio Herencia

---
+++ src/com/javabasico/herencia/Oviparo.java
@@ -0,0 +1,4 @@
+package com.javabasico.herencia;
+
+public class Oviparo extends Animal{
+}
--- src/com/javabasico/herencia/Test.java
+++ src/com/javabasico/herencia/Test.java
@@ -2,6 +2,6 @@ package com.javabasico.herencia;
 
 public class Test {
     public static void main(String[] args) {
-
+        Animal animal1 = Animal();
     }
 }
