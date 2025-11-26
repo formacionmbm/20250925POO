f623fe80afbf15601d23d35a0968f42a08ff0dc3
Mañanas <manianas@gmail.com>
2025-11-26 11:26:55 +0100

    subiendo arbol de herencia

---
+++ src/com/javabasico/herencia/Animal.java
@@ -0,0 +1,17 @@
+package com.javabasico.herencia;
+
+public class Animal {
+    protected double peso;
+
+    public void nacer(){
+        System.out.println("Animal-Nazco!");
+    }
+
+    public void vivir(){
+        System.out.println("Animal-Vivo!");
+    }
+
+    public void morir(){
+        System.out.println("Animal-Muero!");
+    }
+}
