3fd1c04d6c32ade8f4ea62d7757ea012348f56d8
Marcos <viacepeda@gmail.com>
2025-11-26 11:28:57 +0100

    Subo Test.java

--- src/com/javabasico/herencia/Test.java
+++ src/com/javabasico/herencia/Test.java
@@ -2,7 +2,7 @@ package com.javabasico.herencia;
 
 public class Test {
     public static void main(String[] args) {
-        Animal animal1 = Animal();
+        Animal animal1 = new Animal();
     }
 }
 
