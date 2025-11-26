c340a252e7efdcc94653eebe56fc540e146a9bde
fnavas <fnavas.dev@gmail.com>
2025-11-26 12:12:28 +0100

    Creadas todas las clases

--- src/com/javabasico/herencia/Animal.java
+++ src/com/javabasico/herencia/Animal.java
@@ -1,11 +1,21 @@
 package com.javabasico.herencia;
 
+/**
+ * Clase que representa un Animal
+ * @author Fernando Navas
+ * @version 1.0
+ */
 public class Animal {
     private double peso;
 
     public double getPeso() {
         return this.peso;
     }
+
+    /**
+     * Método para cambiar la variable peso
+     * @param peso
+     */
     public void setPeso(double peso) {
         this.peso = peso;
     }
