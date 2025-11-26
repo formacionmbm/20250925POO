72fa456cef142056088e16a9da76ab757ca26fc0
Mananas <fran@>
2025-11-26 11:22:32 +0100

    Creando arbol de herencia

---
+++ src/com/javabasico/herencia/Animal.java
@@ -0,0 +1,16 @@
+package com.javabasico.herencia;
+
+public class Animal {
+    protected double peso;
+
+    public void nacer(){
+        System.out.println("Animal-nazco");
+    }
+    public void vivir(){
+        System.out.println("Animal-vivo");
+    }
+    public void morir(){
+        System.out.println("Animal-muero");
+    }
+
+}
