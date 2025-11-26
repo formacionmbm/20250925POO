83364e05d6ab4b2a62ce389e45772bdeb77f3790
cesar <ejercicios@gmail.com>
2025-11-26 12:22:30 +0100

    arbol

---
+++ src/com/javabasico/herencia/Animal.java
@@ -0,0 +1,19 @@
+package com.javabasico.herencia;
+
+public class Animal {
+
+    protected double peso;
+
+    public void nacer(){
+        System.out.println("ANIMAL - nace");
+    }
+
+    public void vivir(){
+        System.out.println("ANIMAL - se jode");
+    }
+
+    public void morir(){
+        System.out.println("ANIMAL - descansa");
+    }
+
+}
