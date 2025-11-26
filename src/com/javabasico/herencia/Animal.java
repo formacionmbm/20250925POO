5fcbda4619fe058d8ff5221ea061521c5bed5c3e
fnavas <fnavas.dev@gmail.com>
2025-11-26 11:22:07 +0100

    Creadas todas las clases

---
+++ src/com/javabasico/herencia/Animal.java
@@ -0,0 +1,24 @@
+package com.javabasico.herencia;
+
+public class Animal {
+    private double peso;
+
+    public double getPeso() {
+        return this.peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void nacer() {
+        System.out.println("ANIMAL-Nazco");
+    }
+
+    public void vivir() {
+        System.out.println("ANIMAL-Vivo");
+    }
+
+    public void morir() {
+        System.out.println("ANIMAL-Morir");
+    }
+}
